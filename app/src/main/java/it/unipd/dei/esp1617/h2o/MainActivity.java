84e4dfd8f9f0a30e6c26ccb1d875e2d4b56e70ba
francescoFabris <francesco.fabris.8@studenti.unipd.it>
2017-04-15 16:45:54 +0200

    donut graph added

--- app/src/main/java/it/unipd/dei/esp1617/h2o/MainActivity.java
+++ app/src/main/java/it/unipd/dei/esp1617/h2o/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button bu;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton faplus, faminus;
     private static int drunkGlasses;
     private boolean toastNegSent;
+    private ImageView imageMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        drunkGlasses=preferences.getInt("drunk_glasses",0);
+        drunkGlasses = preferences.getInt("drunk_glasses", 0);
 
-        tv1 = (TextView) findViewById(R.id.textView1);
+
         tv2 = (TextView) findViewById(R.id.textView2);
-        tv3 = (TextView) findViewById(R.id.textView3);
         tv4 = (TextView) findViewById(R.id.textView4);
         changeViewsText();
 
@@ -41,23 +42,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        faplus = (FloatingActionButton)findViewById(R.id.plus_button);
-        faminus = (FloatingActionButton)findViewById(R.id.minus_button);
+        faplus = (FloatingActionButton) findViewById(R.id.plus_button);
+        faminus = (FloatingActionButton) findViewById(R.id.minus_button);
         //Incremento del numero di bicchieri bevuti dall'utente
-        faplus.setOnClickListener(new View.OnClickListener(){
+        faplus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 incrementGlasses();
             }
         });
         //Decremento del numero di bicchieri bevuti dall'utente
-        faminus.setOnClickListener(new View.OnClickListener(){
+        faminus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 decrementGlasses();
             }
         });
+
+        imageMan = (ImageView) findViewById(R.id.donut);
+
     }
+
 
     @Override
     protected void onPause(){
@@ -91,9 +96,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void changeViewsText(){
-        tv2.setText(Integer.toString(drunkGlasses));
+        int drunkMl = drunkGlasses*200;
+        String toShow = Integer.toString(drunkMl) + " ml";
+        tv2.setText(toShow);
         tv4.setText((drunkGlasses>5)?R.string.c2:R.string.c1);
+        if(0 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man1);
+        }
+        if(1 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man2);
+        }
+        if(2 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man3);
+        }
+        if(3 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man4);
+        }
+ //       if(4 == drunkGlasses) {
+  //          imageMan.setImageResource(R.drawable.man5);  // BUG (non so perchè)
+   //     }
+        if(5 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man6);
+        }
+        if(6 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man7);
+        }
+        if(7 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man8);
+        }
+        if(8 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man9);
+        }
+        if(9 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man10);
+        }
+        if(10 == drunkGlasses) {
+            imageMan.setImageResource(R.drawable.man11);
+        }
+
     }
+
 
     private boolean isToastNegSent(){
         return toastNegSent;
